7372b340657c6f55e4e82357466221f8a7dac9bc
Patrick Batista de Arruda <patrick.batista.a@hotmail.com>
2019-10-10 16:23:44 -0300

    Aplicando arc4random e animação em button

--- gerador de numeros/ViewController.swift
+++ gerador de numeros/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        let numero = arc4random_uniform(11)
+        legendaResultado.text = String(numero)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
