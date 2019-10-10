c400fd90ce1a64f8b5d32bbedd45c16799b2fbf1
Patrick Batista de Arruda <patrick.batista.a@hotmail.com>
2019-10-10 15:28:49 -0300

    Initial Commit

---
+++ gerador de numeros/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gerador de numeros
+//
+//  Created by Patrick Batista de Arruda on 10/10/19.
+//  Copyright © 2019 Udemy IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
